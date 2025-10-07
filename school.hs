4a5077ac618b305d76ec5f918e90fc1b0c0d094e
ProrokCZ13 <vojtech.cerny.cze@gmail.com>
2025-10-07 16:26:20 +0200

    modified:   school.hs

--- school.hs
+++ school.hs
@@ -1,3 +1,7 @@
+-- :l <soubor> => načtení scriptu
+-- :r <soubor> => znovunačtení scriptu
+
+
 soucetCtveru a b = (a ^ 2) + (b ^ 2)
 
 jeMensi a b = a < b
@@ -8,17 +12,19 @@ logickySoucet a b = if a > 0 then True else if b > 0 then True else False
 
 
 --vzory (patter matching)
-ligickySoucin False False = False
-ligickySoucin _ _ = True
+ligickySoucet False False = False
+ligickySoucet True _ = True
 --ligickySoucin True False = True
 --ligickySoucin True True = True
 
 
+logickySoucin True True = True
+logickySoucin _ _ = False
+
+ligickySoucin a b = if a == True then if b == True then True else False else False
 
 
+absoulutniHodnota x = if x < 0 then -x else x
 
+sign x = if x > 0 then 1 else if x == 0 then 0 if x < 0 then True else False
 
-
-
-
---This is 
